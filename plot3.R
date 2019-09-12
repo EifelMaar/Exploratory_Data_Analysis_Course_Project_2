b3422692a4cc812e24e085c37d4619e7bc09eccc
EifelMaar <50993100+EifelMaar@users.noreply.github.com>
2019-09-12 13:13:20 +0200

    Question 3

--- plot3.R
+++ plot3.R
@@ -8,8 +8,9 @@ if( !exists("dfSCC") ) # read only if not already exists
   dfSCC <- readRDS("./Source_Classification_Code.rds")
 }
 
-## Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == "24510") from 1999 to 2008? 
-## Use the base plotting system to make a plot answering this question.
+## Of the four types of sources indicated by the type (point, nonpoint, onroad, nonroad) variable, 
+## which of these four sources have seen decreases in emissions from 1999–2008 for Baltimore City? 
+## Which have seen increases in emissions from 1999–2008? Use the ggplot2 plotting system to make a plot answer this question.
 
 dfNEIBaltimore <- dfNEI[dfNEI$fips=="24510", ]
 
